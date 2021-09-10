93dd1734570705f4c46046771308256dde17faf7
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-09-10 13:39:16 +0300

    Add basic Django project
    Created a new Django project based on the latest Django,
    Django REST Framework, PostGIS, pip-tools, and the usual
    code quality tools (flake8, black, isort), and whitenoise
    for hosting static files (mostly for admin interface).

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # geo-search
+
 Service for searching geospatial information
+
+## Development without Docker
+
+Prerequisites:
+
+* PostgreSQL 11 or higher with PostGIS extension
+* Python 3.9
+
+### Installing Python requirements
+
+* Run `pip install -r requirements.txt`
+* Run `pip install -r requirements-dev.txt` (development requirements)
+
+### Database
+
+To setup a database compatible with default database settings:
+
+Create user and database
+
+    sudo -u postgres createuser -P -R -S geo-search  # use password `geo-search`
+    sudo -u postgres createdb -O geo-search geo-search
+
+Allow user to create test database
+
+    sudo -u postgres psql -c 'ALTER USER "geo-search" CREATEDB;'
+
+Create the PostGIS extension if needed
+
+    sudo -u postgres psql -c 'CREATE EXTENSION postgis;'
+
+## Keeping Python requirements up to date
+
+1. Add new packages to `requirements.in` or `requirements-dev.in`
+2. Update `.txt` file for the changed requirements file:
+    * `pip-compile requirements.in`
+    * `pip-compile requirements-dev.in`
+3. If you want to update dependencies to their newest versions, run:
+    * `pip-compile --upgrade requirements.in requirements-dev.in`
+4. To install Python requirements run:
+    * `pip-sync requirements.txt requirements-dev.txt`
+
+## Code format
+
+This project uses
+[`black`](https://github.com/psf/black),
+[`flake8`](https://gitlab.com/pycqa/flake8) and
+[`isort`](https://github.com/PyCQA/isort)
+for code formatting and quality checking. Project follows the basic
+black config, without any modifications.
+
+Basic `black` commands:
+
+* To let `black` do its magic: `black .`
+* To see which files `black` would change: `black --check .`
---
+++ geo_search/settings.py
@@ -0,0 +1,87 @@
+from django.utils.translation import gettext_lazy as _
+from environ import Env
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, ""),
+    ALLOWED_HOSTS=(list, []),
+    STATIC_ROOT=(str, str(BASE_DIR / "static")),
+    STATIC_URL=(str, "/static/"),
+    DATABASE_URL=(
+        str,
+        "postgis://geo-search:geo-search@localhost:5432/geo-search",
+    ),
+)
+
+Env.read_env(BASE_DIR / ".env")
+
+DEBUG = env.bool("DEBUG")
+SECRET_KEY = env.str("SECRET_KEY")
+if DEBUG and not SECRET_KEY:
+    SECRET_KEY = "secret-for-debugging-only"
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+# Application definition
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "corsheaders",
+    "rest_framework",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+ROOT_URLCONF = "geo_search.urls"
+
+WSGI_APPLICATION = "geo_search.wsgi.application"
+
+# Databases
+DATABASES = {"default": env.db()}
+
+# Internationalization
+LANGUAGE_CODE = "fi"
+LANGUAGES = (("fi", _("Finnish")), ("en", _("English")), ("sv", _("Swedish")))
+TIME_ZONE = "Europe/Helsinki"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_ROOT = env.str("STATIC_ROOT")
+STATIC_URL = env.str("STATIC_URL")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
