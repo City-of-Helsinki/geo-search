8b17a8afb713754b9799684f1f0c9ae40e647453
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-09-16 08:32:52 +0300

    Add admin form for addresses
    Using the default admin form has poor performance
    with django-parler. This one follows the performance
    guidelines:
    
    https://django-parler.readthedocs.io/en/stable/performance.html#query-prefetching

--- address/admin.py
+++ address/admin.py
@@ -1,21 +1,34 @@
 from django.contrib import admin
+from django.forms import ModelForm
 from parler.admin import TranslatableAdmin
 
 from .models import Address, Municipality, Street
 
 
+class AddressAdminForm(ModelForm):
+    """
+    An admin form for addresses that prefetches translations.
+    This is need to keep the Django admin interface responsive
+    when there are thousands of addresses.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields["street"].queryset = self.fields[
+            "street"
+        ].queryset.prefetch_related("translations")
+
+
+@admin.register(Municipality)
 class MunicipalityAdmin(TranslatableAdmin):
     pass
 
 
+@admin.register(Street)
 class StreetAdmin(TranslatableAdmin):
     pass
 
 
+@admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Municipality, MunicipalityAdmin)
-admin.site.register(Street, StreetAdmin)
-admin.site.register(Address, AddressAdmin)
+    form = AddressAdminForm
