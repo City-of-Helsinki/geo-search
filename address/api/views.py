b031b1769c38422ddbf66e987d3b46bc77751016
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-09-17 15:31:17 +0300

    Add address filtering
    Allows filtering by street name, number, number end,
    letter, municipality, and postal code. If the field is
    translated, then we will search from all translations.

--- address/api/views.py
+++ address/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q, QuerySet
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from ..models import Address
@@ -7,3 +8,49 @@ from .serializers import AddressSerializer
 class AddressViewSet(ReadOnlyModelViewSet):
     queryset = Address.objects.order_by("pk")
     serializer_class = AddressSerializer
+
+    def get_queryset(self) -> QuerySet:
+        addresses = self.queryset
+        addresses = self._filter_by_street_name(addresses)
+        addresses = self._filter_by_street_number(addresses)
+        addresses = self._filter_by_street_letter(addresses)
+        addresses = self._filter_by_municipality(addresses)
+        addresses = self._filter_by_postal_code(addresses)
+        # We have to do distinct here because filtering by the translated
+        # fields can return the same object multiple times if it has multiple
+        # translations (e.g. "fi" and "sv").
+        return addresses.distinct()
+
+    def _filter_by_street_name(self, addresses: QuerySet) -> QuerySet:
+        street_name = self.request.query_params.get("streetname")
+        if street_name is None:
+            return addresses
+        return addresses.filter(street__translations__name__iexact=street_name)
+
+    def _filter_by_street_number(self, addresses: QuerySet) -> QuerySet:
+        street_number = self.request.query_params.get("streetnumber")
+        if street_number is None:
+            return addresses
+        return addresses.filter(
+            Q(number__iexact=street_number) | Q(number_end__iexact=street_number)
+        )
+
+    def _filter_by_street_letter(self, addresses: QuerySet) -> QuerySet:
+        street_letter = self.request.query_params.get("streetletter")
+        if street_letter is None:
+            return addresses
+        return addresses.filter(letter__iexact=street_letter)
+
+    def _filter_by_municipality(self, addresses: QuerySet) -> QuerySet:
+        municipality = self.request.query_params.get("municipality")
+        if municipality is None:
+            return addresses
+        return addresses.filter(
+            street__municipality__translations__name__iexact=municipality
+        )
+
+    def _filter_by_postal_code(self, addresses: QuerySet) -> QuerySet:
+        postal_code = self.request.query_params.get("postalcode")
+        if postal_code is None:
+            return addresses
+        return addresses.filter(postal_code__iexact=postal_code)
--- address/tests/test_api_views.py
+++ address/tests/test_api_views.py
@@ -3,7 +3,7 @@ from pytest import mark
 from rest_framework.test import APIClient
 
 from ..api.serializers import AddressSerializer
-from ..tests.factories import AddressFactory
+from ..tests.factories import AddressFactory, MunicipalityFactory, StreetFactory
 
 
 @mark.django_db
@@ -18,4 +18,102 @@ def test_get_address_list():
         "next": None,
         "previous": None,
         "results": [serializer.to_representation(address)],
+    }
+
+
+@mark.django_db
+def test_filter_addresses_by_street_name():
+    api_client = APIClient()
+    match = AddressFactory(street=StreetFactory(name="Matching Street"))
+    AddressFactory(street=StreetFactory(name="Other Street"))
+    serializer = AddressSerializer()
+    response = api_client.get(
+        reverse("address:address-list"), {"streetname": match.street.name}
+    )
+    assert response.status_code == 200
+    assert response.data == {
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": [serializer.to_representation(match)],
+    }
+
+
+@mark.django_db
+def test_filter_addresses_by_street_number():
+    api_client = APIClient()
+    matching_number = 42
+    matches = [
+        AddressFactory(number=matching_number, number_end="99"),
+        AddressFactory(number="99", number_end=matching_number),
+    ]
+    AddressFactory(number="99", number_end="99")
+    AddressFactory(number="99", number_end="99")
+    serializer = AddressSerializer()
+    response = api_client.get(
+        reverse("address:address-list"), {"streetnumber": matching_number}
+    )
+    assert response.status_code == 200
+    assert response.data == {
+        "count": 2,
+        "next": None,
+        "previous": None,
+        "results": [serializer.to_representation(match) for match in matches],
+    }
+
+
+@mark.django_db
+def test_filter_addresses_by_street_letter():
+    api_client = APIClient()
+    matching_letter = "M"
+    match = AddressFactory(letter=matching_letter)
+    AddressFactory(letter="XX")
+    serializer = AddressSerializer()
+    response = api_client.get(
+        reverse("address:address-list"), {"streetletter": matching_letter}
+    )
+    assert response.status_code == 200
+    assert response.data == {
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": [serializer.to_representation(match)],
+    }
+
+
+@mark.django_db
+def test_filter_addresses_by_municipality():
+    api_client = APIClient()
+    municipality = MunicipalityFactory(name="Match")
+    street = StreetFactory(municipality=municipality)
+    match = AddressFactory(street=street)
+    AddressFactory()
+    serializer = AddressSerializer()
+    response = api_client.get(
+        reverse("address:address-list"), {"municipality": municipality.name}
+    )
+    assert response.status_code == 200
+    assert response.data == {
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": [serializer.to_representation(match)],
+    }
+
+
+@mark.django_db
+def test_filter_addresses_by_postal_code():
+    api_client = APIClient()
+    match = AddressFactory(postal_code="00100")
+    AddressFactory(postal_code="99999")
+    serializer = AddressSerializer()
+    response = api_client.get(
+        reverse("address:address-list"), {"postalcode": match.postal_code}
+    )
+    assert response.status_code == 200
+    assert response.data == {
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": [serializer.to_representation(match)],
     }
