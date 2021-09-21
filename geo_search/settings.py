d756d6c535537aca5272c145b4cf119ebf12e053
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-09-21 13:06:26 +0300

    Add ability to get responses in XML format
    By default, the API returns JSON responses. If we browse the API with the
    browser, the browsable API will be shown instead. With the `&format=xxx`
    you can specify the format explicitly, so to get the data in XML format,
    you can add `&format=xml` to the end of the URL.

--- geo_search/settings.py
+++ geo_search/settings.py
@@ -94,6 +94,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 PROJECTION_SRID = 4326  # WGS84
 
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
