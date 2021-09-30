e25b2b8a9e3f428dd638dff8a91dafb783e2c0c3
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-09-30 12:04:09 +0300

    Add health check and readiness URLs
    These endpoints follow the same practice as other City of Helsinki
    projects, except the readiness check also connects to the database
    and tries to execute a simple query.
    
    Both endpoints are excluded from uWSGI logging as with other projects.

--- geo_search/urls.py
+++ geo_search/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 from address import urls as addresses_urls
@@ -7,3 +8,28 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/", include((addresses_urls, "address"), namespace="v1/address")),
 ]
+
+
+def healthz(*args, **kwargs) -> HttpResponse:
+    """Returns status code 200 if the server is alive."""
+    return HttpResponse(status=200)
+
+
+def readiness(*args, **kwargs) -> HttpResponse:
+    """
+    Returns status code 200 if the server is ready to perform its duties.
+
+    This goes through each database connection and perform a standard SQL
+    query without requiring any particular tables to exist.
+    """
+    from django.db import connections
+
+    for name in connections:
+        cursor = connections[name].cursor()
+        cursor.execute("SELECT 1;")
+        cursor.fetchone()
+
+    return HttpResponse(status=200)
+
+
+urlpatterns += [path("healthz", healthz), path("readiness", readiness)]
