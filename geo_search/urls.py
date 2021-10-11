0ac2f745b22ba138545419fb924552456a24ae26
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-10-11 15:56:07 +0300

    Add OpenAPI schema and documentation
    The OpenAPI schema can be downloaded at /schema/ and the
    documentation at the /schema/docs/ endpoint.

--- geo_search/urls.py
+++ geo_search/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from address import urls as addresses_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/", include((addresses_urls, "address"), namespace="v1/address")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="schema-docs",
+    ),
 ]
 
 
