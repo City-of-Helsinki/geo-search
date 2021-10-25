2b4090bde3559fc293e06bde9661ae5274f2373a
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-10-25 15:35:36 +0300

    Add permission for REST API
    To use any REST endpoint, the user has to be logged in (e.g.
    via the Django admin interface), or the request must have an
    API key provided in the `Authorization` header.

---
+++ geo_search/tests/test_permissions.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from pytest import mark
+from rest_framework.test import APIClient
+from rest_framework_api_key.models import APIKey
+
+
+def test_anonymous_client_cannot_access_api_without_api_key():
+    api_client = APIClient()
+    response = api_client.get("/v1/")
+    assert response.status_code == 403
+
+
+@mark.django_db
+def test_anonymous_client_can_access_api_with_valid_api_key():
+    valid_api_key = APIKey.objects.create_key(name="test")[-1]
+    api_client = APIClient(HTTP_AUTHORIZATION=f"Api-Key {valid_api_key}")
+    response = api_client.get("/v1/")
+    assert response.status_code == 200
+
+
+@mark.django_db
+def test_anonymous_client_cannot_access_api_with_revoked_api_key():
+    revoked_api_key = APIKey.objects.create_key(name="test", revoked=True)[-1]
+    api_client = APIClient(HTTP_AUTHORIZATION=f"Api-Key {revoked_api_key}")
+    response = api_client.get("/v1/")
+    assert response.status_code == 403
+
+
+@mark.django_db
+def test_authenticated_client_can_access_api():
+    user = User.objects.create()
+    api_client = APIClient()
+    api_client.force_authenticate(user)
+    response = api_client.get("/v1/")
+    assert response.status_code == 200
