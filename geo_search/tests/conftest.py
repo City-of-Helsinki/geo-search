1b5fdf365653c43b7b05cfe4cdc93ab44dbb7d9c
Jarkko Piiroinen <jarkko.piiroinen@anders.com>
2021-11-03 10:22:34 +0200

    Fix tests failing without authorization requirement

--- geo_search/tests/conftest.py
+++ geo_search/tests/conftest.py
@@ -2,5 +2,10 @@ from pytest import fixture
 
 
 @fixture
+def authorization_required(settings):
+    settings.REQUIRE_AUTHORIZATION = True
+
+
+@fixture
 def no_authorization_required(settings):
     settings.REQUIRE_AUTHORIZATION = False
