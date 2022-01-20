d200e06223938e456d13ae9ab94e9dad149216d0
Mika Hietanen <m.hietanen84@gmail.com>
2022-01-20 10:19:53 +0200

    Unquote next- and previous-links

--- geo_search/pagination.py
+++ geo_search/pagination.py
@@ -1,5 +1,18 @@
 from rest_framework.pagination import PageNumberPagination
+from urllib.parse import unquote
 
 
 class Pagination(PageNumberPagination):
     page_size_query_param = "page_size"
+
+    def get_next_link(self):
+        link = super().get_next_link()
+        if link:
+            link = unquote(link)
+        return link
+
+    def get_previous_link(self):
+        link = super().get_previous_link()
+        if link:
+            link = unquote(link)
+        return link
