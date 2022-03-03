5ca5b0af554efe87b923314d33699475176a6089
juuso-j <juuso.jokiniemi@turku.fi>
2022-03-03 11:04:12 +0200

    Now imports postal codes and offices for the province given as argument
    Chooses the bbox to be used when importing by the given province name. Currently supports provinces "uusimaa" and "varsinais-suomi".
    Checks also if argument exists or is wrong.

--- scripts/import-paavo-data.sh
+++ scripts/import-paavo-data.sh
@@ -1,12 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-# URL to Paavo WFS service
-DATA_URL="http://geo.stat.fi/geoserver/wfs?SERVICE=wfs&version=1.0.0&request=GetFeature&srsName=EPSG:3067&outputFormat=SHAPE-ZIP&typeNames=pno_meri_2021&bbox=267392.57814054575,6636189.158504381,476288.57814054575,6747293.158504381"
+available_provinces="'uusimaa' and 'varisinais-suomi'"
 
-# Directory where the data will be downloaded and extracted
-DATA_DIR=/tmp/paavo
+if [ $# == 0 ]
+then
+    echo "No province argument supplied."    
+    echo "Available provinces are ${available_provinces}."
+    exit 0
+fi
+
+declare -A bbox
+bbox["varsinais-suomi"]="125189.83,6611707.59,334187.32,6781558.98"
+bbox["uusimaa"]="267392.57814054575,6636189.158504381,476288.57814054575,6747293.158504381"
+
+if [ ${bbox[$1]+_} ] 
+then 
+    echo "Importing Paavo data for province $1."; 
+else 
+    echo "Province $1 not found, available provinces are ${available_provinces}."; 
+    exit 0
+fi
+
+# URL to Paavo WFS service
+DATA_URL="http://geo.stat.fi/geoserver/wfs?SERVICE=wfs&version=1.0.0&request=GetFeature&srsName=EPSG:3067&outputFormat=SHAPE-ZIP&typeNames=pno_meri_2021&bbox=${bbox[$1]}"
+
+DATA_DIR=/home/juuso/repos/geo-search/scripts/tmp/paavo
 
 # The shapefiles will be extracted to this directory
 EXTRACTED_DIR=$DATA_DIR/extracted
