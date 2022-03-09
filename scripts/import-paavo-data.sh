70c64ad165fb007c28481749f386675d46fd6962
Mika Hietanen <m.hietanen84@gmail.com>
2022-03-09 08:40:54 +0200

    Change imports to use shell scripts
    Bash is not available everywhere, therefore changed existing
    bash-scripts to be standard shell-scripts.

--- scripts/import-paavo-data.sh
+++ scripts/import-paavo-data.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-
 available_provinces="'uusimaa' and 'varsinais-suomi'"
 
 if [ $# == 0 ]
@@ -11,20 +10,21 @@ then
     exit 0
 fi
 
-declare -A bbox
-bbox["varsinais-suomi"]="125189.83,6611707.59,334187.32,6781558.98"
-bbox["uusimaa"]="267392.57814054575,6636189.158504381,476288.57814054575,6747293.158504381"
-
-if [ ${bbox[$1]+_} ]
+if [ $1 == "uusimaa" ]
 then
-    echo "Importing Paavo data for province $1.";
+    bbox="267392.57814054575,6636189.158504381,476288.57814054575,6747293.158504381"
+elif [ $1 == "varsinais-suomi" ]
+then
+    bbox="125189.83,6611707.59,334187.32,6781558.98"
 else
     echo "Province $1 not found, available provinces are ${available_provinces}.";
     exit 0
 fi
 
+echo "Importing Paavo data for province $1.";
+
 # URL to Paavo WFS service
-DATA_URL="http://geo.stat.fi/geoserver/wfs?SERVICE=wfs&version=1.0.0&request=GetFeature&srsName=EPSG:3067&outputFormat=SHAPE-ZIP&typeNames=pno_meri_2022&bbox=${bbox[$1]}"
+DATA_URL="http://geo.stat.fi/geoserver/wfs?SERVICE=wfs&version=1.0.0&request=GetFeature&srsName=EPSG:3067&outputFormat=SHAPE-ZIP&typeNames=pno_meri_2022&bbox=${bbox}"
 
 DATA_DIR=/tmp/paavo
 
