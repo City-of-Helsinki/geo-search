0fc5d0d78935a995967ace299f1a654948f12969
Mika Hietanen <m.hietanen84@gmail.com>
2026-02-16 12:28:12 +0200

    feat: update model verbose names and translations
    Refs: RATYK-154

--- address/models.py
+++ address/models.py
@@ -5,24 +5,27 @@ from parler.models import TranslatableModel, TranslatedFields
 
 
 class Municipality(TranslatableModel):
-    id = models.CharField(max_length=100, primary_key=True)
-    code = models.CharField(max_length=3)
+    id = models.CharField(_("Id"), max_length=100, primary_key=True)
+    code = models.CharField(_("Municipality code"), max_length=3)
     translations = TranslatedFields(
         name=models.CharField(_("Name"), max_length=100, db_index=True)
     )
     area = models.MultiPolygonField(
-        srid=settings.PROJECTION_SRID, null=True, blank=True
+        _("Area"), srid=settings.PROJECTION_SRID, null=True, blank=True
     )
 
     def __str__(self) -> str:
         return self.name
 
     class Meta:
+        verbose_name = _("Municipality")
         verbose_name_plural = _("Municipalities")
 
 
 class Street(TranslatableModel):
-    municipality = models.ForeignKey(Municipality, models.CASCADE, db_index=True)
+    municipality = models.ForeignKey(
+        Municipality, models.CASCADE, db_index=True, verbose_name=_("Municipality")
+    )
     modified_at = models.DateTimeField(auto_now=True)
     translations = TranslatedFields(
         name=models.CharField(_("Name"), max_length=100, db_index=True),
@@ -31,42 +34,61 @@ class Street(TranslatableModel):
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = _("Street")
+        verbose_name_plural = _("Streets")
+
 
 class PostalCodeArea(TranslatableModel):
-    postal_code = models.CharField(max_length=5, null=True, blank=True)
+    postal_code = models.CharField(
+        _("Postal code"), max_length=5, null=True, blank=True
+    )
     municipality = models.ForeignKey(
-        Municipality, models.CASCADE, db_index=True, null=True, blank=True
+        Municipality,
+        models.CASCADE,
+        db_index=True,
+        null=True,
+        blank=True,
+        verbose_name=_("Municipality"),
     )
     translations = TranslatedFields(
         name=models.CharField(_("Name"), max_length=100, null=True, blank=True)
     )
     area = models.MultiPolygonField(
-        srid=settings.PROJECTION_SRID, null=True, blank=True
+        _("Area"), srid=settings.PROJECTION_SRID, null=True, blank=True
     )
 
     def __str__(self) -> str:
         return self.postal_code
 
     class Meta:
+        verbose_name = _("Postal code area")
         verbose_name_plural = _("Postal code areas")
 
 
 class Address(models.Model):
-    municipality = models.ForeignKey(Municipality, models.CASCADE, db_index=True)
-    street = models.ForeignKey(
-        Street, models.CASCADE, db_index=True, related_name="addresses"
+    municipality = models.ForeignKey(
+        Municipality, models.CASCADE, db_index=True, verbose_name=_("Municipality")
     )
-    number = models.CharField(max_length=6, blank=True)
-    number_end = models.CharField(max_length=6, blank=True)
-    letter = models.CharField(max_length=2, blank=True)
+    street = models.ForeignKey(
+        Street,
+        models.CASCADE,
+        db_index=True,
+        related_name="addresses",
+        verbose_name=_("Street"),
+    )
+    number = models.CharField(_("Number"), max_length=6, blank=True)
+    number_end = models.CharField(_("Number end"), max_length=6, blank=True)
+    letter = models.CharField(_("Letter"), max_length=2, blank=True)
     postal_code_area = models.ForeignKey(
         PostalCodeArea,
         models.CASCADE,
         null=True,
         blank=True,
         related_name="addresses",
+        verbose_name=_("Postal code area"),
     )
-    location = models.PointField(srid=settings.PROJECTION_SRID)
+    location = models.PointField(_("Location"), srid=settings.PROJECTION_SRID)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
@@ -78,6 +100,7 @@ class Address(models.Model):
         return f"{s}, {self.municipality}"
 
     class Meta:
+        verbose_name = _("Address")
         verbose_name_plural = _("Addresses")
         indexes = [
             # This index speeds up address?municipality and address?municipalitycode
