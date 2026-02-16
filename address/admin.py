d9355a8baec0c3339757fc0a69836bc21b39817a
Mika Hietanen <m.hietanen84@gmail.com>
2026-02-16 12:30:49 +0200

    feat: update Django Admin configs
    Update list display, ordering and search fields for:
    - MunicipalityAdmin
    - StreetAdmin
    - PostalCodeAreaAdmin
    - AddressAdmin
    
    Refs: RATYK-154

--- address/admin.py
+++ address/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from parler.admin import TranslatableAdmin
 
 from .models import Address, Municipality, PostalCodeArea, Street
@@ -6,19 +7,40 @@ from .models import Address, Municipality, PostalCodeArea, Street
 
 @admin.register(Municipality)
 class MunicipalityAdmin(TranslatableAdmin):
-    pass
+    list_display = ("id", "code")
+    ordering = ("id",)
+    search_fields = ("id",)
 
 
 @admin.register(Street)
 class StreetAdmin(TranslatableAdmin):
-    pass
+    list_display = ("name_column", "municipality")
+    search_fields = ("translations__name",)
+
+    @admin.display(description=_("Name"))
+    def name_column(self, object):
+        return object.name
+
+    def get_queryset(self, request):
+        language_code = self.get_queryset_language(request)
+        return (
+            super()
+            .get_queryset(request)
+            .translated(language_code)
+            .order_by("translations__name")
+        )
 
 
 @admin.register(PostalCodeArea)
 class PostalCodeAreaAdmin(TranslatableAdmin):
-    pass
+    list_display = ("postal_code", "name", "municipality")
+    ordering = ("postal_code",)
+    search_fields = ("postal_code",)
 
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
+    list_display = ("street", "number", "municipality", "postal_code_area")
     raw_id_fields = ["street"]
+    ordering = ("street", "number", "municipality")
+    search_fields = ("street__translations__name",)
